f1cfa9460c7b52ae77065c63ea5cfae9ad55d8c7
Hediye Yildirim <hediyeyldrm0@gmail.com>
2022-11-05 20:29:14 +0300

    page "404" edited

--- src/Pages/NoPage.jsx
+++ src/Pages/NoPage.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import NotFound from "../img/404.gif";
 
 export default function NoPage() {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <img className="nopageImg"
-          src="https://www.myphukettravel.com/assets/front-end/images/404.gif"
-          alt="404"
-        />
+        <img className="nopageImg" src={NotFound} alt="404" />
         <button className="nopageButton">
           <Link to="/register">Register</Link>
         </button>
