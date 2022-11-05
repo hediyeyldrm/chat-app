fbcde14785396ebe47f04eae9ed4c60fe19a5194
Hediye Yildirim <hediyeyldrm0@gmail.com>
2022-11-05 16:23:19 +0300

    "404" page added

---
+++ src/Pages/NoPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function NoPage() {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <img
+          src="https://www.myphukettravel.com/assets/front-end/images/404.gif"
+          alt="404"
+        />
+        <button className="nopageButton">
+          <Link to="/register">Register</Link>
+        </button>
+      </div>
+    </div>
+  );
+}
