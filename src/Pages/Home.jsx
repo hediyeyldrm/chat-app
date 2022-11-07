0448329eab8c70f3ff810ea86c395bde609c48b5
Hediye Yildirim <hediyeyldrm0@gmail.com>
2022-11-07 13:19:42 +0300

    edited

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../style.scss";
 import Sidebar from "../Components/Sidebar";
 import Chat from "../Components/Chat";
+import { themeContext } from "../context/ThemeContext";
 
 export default function Home() {
+  const { theme } = useContext(themeContext);
   return (
-    <div className="home">
+    <div className={`home ${theme}`}>
       <div className="container">
         <Sidebar />
         <Chat />
