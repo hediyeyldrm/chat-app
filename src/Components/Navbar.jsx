b2cdc884d57ba505fd71f42f8406a74f336f50d9
Hediye Yildirim <hediyeyldrm0@gmail.com>
2022-11-07 13:19:16 +0300

    component edits added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,17 +3,22 @@ import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { themeContext } from "../context/ThemeContext";
 
 export default function Navbar() {
   const { currentUser } = useContext(AuthContext);
+  const { toggleTheme } = useContext(themeContext);
 
   return (
     <div className="navbar">
-      <span className="logo">Chat App</span>
+      <span className="logo">Hii Chat</span>
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>Logout</button>
+        <button className="theme" onClick={toggleTheme}>
+          <i className="fa-solid fa-moon"></i>
+        </button>
       </div>
     </div>
   );
