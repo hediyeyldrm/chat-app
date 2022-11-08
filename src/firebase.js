e81986868367ffce82ff1afdcb975424a4d0ac23
Hediye Yildirim <hediyeyldrm0@gmail.com>
2022-11-09 02:03:25 +0300

    app

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB3T7P9tdp2E7nEq3-qbuIRwc_Nr74jZW0",
-  authDomain: "chatwebsite-bd0bd.firebaseapp.com",
-  projectId: "chatwebsite-bd0bd",
-  storageBucket: "chatwebsite-bd0bd.appspot.com",
-  messagingSenderId: "824639707998",
-  appId: "1:824639707998:web:1e465c693e63eee3a84dcf",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
