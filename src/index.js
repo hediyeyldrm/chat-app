6e0e088ba16ea776e164ccb10720d721305785c7
Hediye Yildirim <hediyeyldrm0@gmail.com>
2022-11-09 01:01:28 +0300

    app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ChatContextProvider } from "./context/ChatContext";
 import { ThemeProvider } from "./context/ThemeContext";
+import "./i18n";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,7 +13,9 @@ root.render(
     <ChatContextProvider>
       <ThemeProvider>
         <React.StrictMode>
-          <App />
+          <Suspense fallback={<div>Loading...</div>}>
+            <App />
+          </Suspense>
         </React.StrictMode>
       </ThemeProvider>
     </ChatContextProvider>
